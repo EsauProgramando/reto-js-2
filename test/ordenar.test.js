ae63f1eece88369b742c62e8535f31cc31e9fd82
unknown <wsegurar@gmail.com>
2021-04-28 23:22:11 -0500

    correccion de prueba

--- test/ordenar.test.js
+++ test/ordenar.test.js
@@ -1,7 +1,7 @@
 
 const obtNumber = require('../src/ordenar');
 
-test('validando la prueba unitaria de la funcion hello-world', () => {//
+test('validando la prueba unitaria de la funcion obtNumber', () => {//
   const numbers = [2,34,67,12,4,43,28]   
   expect(obtNumber(numbers)).toEqual(67);
 });
