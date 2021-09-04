37b9df78edc069874747b69823204efb6f2e8291
EsauProgramando <89734246+EsauProgramando@users.noreply.github.com>
2021-09-03 21:53:03 -0500

    corregido

--- src/ordenar.js
+++ src/ordenar.js
@@ -2,10 +2,22 @@
   function obtNumber (array = []) {   
     let numMayor = 0   
     array.forEach((item) => {     
-      if (item < numMayor) {       
+      if (item > numMayor) {       
         item = item + 1
         numMayor = item    
        }   })   
        return numMayor 
   }  
 module.exports = obtNumber;
+
+// corregido el programa
+  function obtNumber (array = [ 8 , 80, 40]) {   
+    let numMayor = 0   
+    array.forEach((item) => {     
+      if (item > numMayor) {       
+        item = item + 1
+        numMayor = item    
+       }   })   
+       return numMayor 
+  }   
+console.log(obtNumber()-1);
